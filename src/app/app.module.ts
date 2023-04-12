1c7df7e3f40e761a25dd5c73de3854ab97d83ff5
Dineshkumar <sethudinesh1705@gmail.com>
2023-04-12 16:56:54 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { navigationroutingmodule } from './navigation/navigation-routing.module';
+
+@NgModule({
+  declarations: [AppComponent, NavigationComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    navigationroutingmodule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
